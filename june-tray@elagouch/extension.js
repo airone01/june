6899fcf2b385089c4b66125850d4bed8f024516d
Erwann Lagouche <21955960+airone01@users.noreply.github.com>
2025-09-28 13:55:52 +0200

    fix(extension): patch loading state

--- june-tray@elagouch/extension.js
+++ june-tray@elagouch/extension.js
@@ -6,6 +6,10 @@ const PopupMenu = imports.ui.popupMenu;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 
+const MSG_STATUS_READY   = "June is ready and loaded!";
+const MSG_STATUS_SYNC    = "June is saving your apps...";
+const MSG_STATUS_STARTUP = "June is loading your apps...";
+
 var JuneTrayIndicator = GObject.registerClass(
 class JuneTrayIndicator extends PanelMenu.Button {
     _init() {
@@ -21,6 +25,13 @@ class JuneTrayIndicator extends PanelMenu.Button {
         this._startupServiceActive = false;
         this._syncServiceActive = false;
 
+        this.syncFramePaths = [];
+        for (let i = 0; i <= 8; i++) {
+            this.syncFramePaths.push(`${Me.dir.get_path()}/icons/sync_${i.toString().padStart(2, '0')}.png`);
+        }
+        this._currentFrame = 0;
+        this._animationTimeoutId = null;
+
         this._createMenu();
         this._updateIcon();
         this._updateMenu();
@@ -29,37 +40,66 @@ class JuneTrayIndicator extends PanelMenu.Button {
     }
 
     _createMenu() {
-        this._statusItem = new PopupMenu.PopupMenuItem('June is ready!', { reactive: false });
+        this._statusItem = new PopupMenu.PopupMenuItem(MSG_STATUS_READY, { reactive: false });
         this._statusItem.label.style_class = 'june-status-header';
         this.menu.addMenuItem(this._statusItem);
     }
 
     _updateMenu() {
         if (this._startupServiceActive) {
-            this._statusItem.label.text = 'June is loading your apps...';
+            this._statusItem.label.text = MSG_STATUS_STARTUP;
             this._statusItem.label.style_class = 'june-status-active';
         } else if (this._syncServiceActive) {
-            this._statusItem.label.text = 'June is saving your apps...';
+            this._statusItem.label.text = MSG_STATUS_SYNC;
             this._statusItem.label.style_class = 'june-status-active';
         } else {
-            this._statusItem.label.text = 'June is ready';
+            this._statusItem.label.text = MSG_STATUS_READY;
             this._statusItem.label.style_class = 'june-status-idle';
         }
     }
 
     _updateIcon() {
         this._icon.remove_style_class_name('june-spinner');
+        this._stopAnimation();
 
-        if (this._startupServiceActive) {
-            this._icon.set_icon_name('folder-download-symbolic');
-        } else if (this._syncServiceActive) {
-            this._icon.set_icon_name('process-working-symbolic');
+        if (this._startupServiceActive || this._syncServiceActive) {
             this._icon.add_style_class_name('june-spinner');
+            this._startAnimation();
         } else {
             this._icon.set_icon_name('package-x-generic-symbolic');
         }
 
         this._updateMenu();
+    }
+
+    _startAnimation() {
+        if (this._animationTimeoutId) return;
+
+        this._setFrame(this._currentFrame);
+
+        const frameDelay = 125; // 125ms per frame for ~8fps (adjust as needed)
+        this._animationTimeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, frameDelay, () => {
+            this._currentFrame = (this._currentFrame + 1) % this.syncFramePaths.length;
+            this._setFrame(this._currentFrame);
+            return GLib.SOURCE_CONTINUE;
+        });
+    }
+
+    _stopAnimation() {
+        if (this._animationTimeoutId) {
+            GLib.source_remove(this._animationTimeoutId);
+            this._animationTimeoutId = null;
+        }
+        this._currentFrame = 0;
+    }
+
+    _setFrame(index) {
+        try {
+            let gicon = Gio.icon_new_for_string(this.syncFramePaths[index]);
+            this._icon.gicon = gicon;
+        } catch (e) {
+            logError(e, 'Failed to load frame ' + index);
+        }
     }
 
     _checkServiceStatus() {
@@ -138,6 +178,7 @@ class JuneTrayIndicator extends PanelMenu.Button {
     }
 
     destroy() {
+        this._stopAnimation();
         this._stopMonitoring();
         super.destroy();
     }
@@ -146,11 +187,16 @@ class JuneTrayIndicator extends PanelMenu.Button {
 class Extension {
     constructor() {
         this._indicator = null;
+        this._theme = null;
+        this._stylesheet = null;
     }
 
     enable() {
-        this._indicator = new JuneTrayIndicator();
+        this._theme = St.ThemeContext.get_for_stage(global.stage).get_theme();
         this._stylesheet = Gio.File.new_for_path(Me.dir.get_path() + '/stylesheet.css');
+        this._theme.load_stylesheet(this._stylesheet);
+
+        this._indicator = new JuneTrayIndicator();
         Main.panel.addToStatusArea('june-tray', this._indicator);
     }
 
@@ -158,6 +204,9 @@ class Extension {
         if (this._indicator) {
             this._indicator.destroy();
             this._indicator = null;
+        }
+        if (this._theme && this._stylesheet) {
+            this._theme.unload_stylesheet(this._stylesheet);
         }
     }
 }
